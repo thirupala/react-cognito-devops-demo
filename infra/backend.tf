64067be081378d4869da570fdcab99ef1de37dd2
thirupala <thirupala@gmail.com>
2025-11-29 11:18:35 +0530

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,1 +1,9 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "react-cognito-terraform-state-727588137294"  # Your account ID
+    key            = "prod/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
